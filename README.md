aabb6afd7d850e1d8cad1020080b24ea2be539dd
Rayoverio <sroliv88@outlook.com>
2025-05-04 22:07:02 -0600

    Se creo el directorio notebooks y dentro el archivo EDA.ipynb. En el directorio principal se edito el contenido de los archivos app.py y README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Bootcamp_P_7
+
+El objetivo de este proyecto fue proporcionarme más oportunidades para practicar las tareas habituales de la ingeniería de software.
+Para lo anterior, aplique lo aprendido en Git y abrí una cuenta en GitHub, generé mi primer entorno virtual y por último realice el codigo para una app web sencilla sobre la realización de un histográma en el archivo "app.py".
+
+A continación dejo enlace a la app:
--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Soy app py")
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Se cargaron las librerias.
+#Se procede a cargar el archivo CSV.
+file_path = r'C:\Users\Oliverio.SILVERIO\Desktop\Bootcamp_P_7\vehicles_us.csv'
+car_data = pd.read_csv(file_path)
+
+#Ahora vamos con la parte de la aplicación
+st.header('Aprendiendo desarrollo Web')
+st.write('Esta aplicación es un breve proyecto para aprender aplicación web')
+
+#Botón
+if st.button('Mostrar Histograma'):
+    fig = px.histogram(car_data, x='odometro')
+    st.plotly_chart(fig)
